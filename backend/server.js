d0de254e546d3d06825c657a2135987959675f0f
jainatshu <mratishayj@gmail.com>
2025-08-24 12:09:16 +0530

    Update code with latest changes

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_ORIGIN || 'https://urvann-4m0o9aut8-atishay-jains-projects-56d9a8a2.vercel.app/',  // your frontend URL
+    origin: '*',  // your frontend URL
     
     credentials: true, // if you're using cookies / auth headers
   })
